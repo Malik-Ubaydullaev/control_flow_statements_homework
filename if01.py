cf9f1f8798d4302d1642668c04e3c665bcf142eb
Your Name <ubaydullayev@samtuit.uz>
2021-12-31 03:35:32 +0500

    1-misol

--- if01.py
+++ if01.py
@@ -1,4 +1,8 @@
 def main(a):
+    if a > 0:
+        a += 1
+    else:
+        a
     """
     If the number is positive, increase it to 1, else leave unchanged.
     Args:
@@ -6,4 +10,4 @@ def main(a):
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """
-    return
+    return a
