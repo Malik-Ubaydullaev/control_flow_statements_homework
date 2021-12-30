63fa8a5a2077d203c99cc38b9b22d24b4d26116b
Your Name <ubaydullayev@samtuit.uz>
2021-12-31 03:39:05 +0500

    3-misol

--- if03.py
+++ if03.py
@@ -1,4 +1,10 @@
 def main(a):
+    if a > 0:
+        a += 1
+    if a < 0:
+        a -= 2
+    else:
+        a = 10
     """
     If the number is positive, increase it to 1, else decrease it to 2. If it is 0, assign 10.
     Args:
@@ -6,4 +12,4 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    return a
