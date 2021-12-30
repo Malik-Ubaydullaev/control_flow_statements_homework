50682d361223bdf6130dbdd809d5aaea4620190f
Your Name <ubaydullayev@samtuit.uz>
2021-12-31 04:25:08 +0500

    8-misol

--- if08.py
+++ if08.py
@@ -1,4 +1,19 @@
 def main(a):
+    message = ''
+    a1 = pow(0, a)
+    b = a // 10
+    a2 = pow(0, b)
+    c = b // 10
+    a3 = pow(0, c)
+    if a % 2 == 0 and (a1 == 0 and a2 == 0 and a3 != 0):
+        message = "two-digit even number"
+    if a % 2 != 0 and (a1 == 0 and a2 == 0 and a3 != 0):
+        message = "two-digit odd number"
+    
+    if a % 2 == 0 and (a1 == 0 and a2 == 0 and a3 == 0):
+        message = "three-digit even number"
+    if a % 2 != 0 and (a1 == 0 and a2 == 0 and a3 == 0):
+        message = "three-digit odd number"
     """
     Given an integer a, check the following conditions:
     "two-digit odd number",
@@ -11,4 +26,4 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    return message
