46960dd77bbd7bd1ab5699f69f805592523448d2
Your Name <ubaydullayev@samtuit.uz>
2021-12-31 04:33:59 +0500

    9-misol

--- if09.py
+++ if09.py
@@ -1,4 +1,14 @@
 def main(a):
+    result = False
+    a1 = a % 10
+    a2 = a // 10
+    b = str(a1) + str(a2)
+    new_a = int(b)
+    
+    if new_a == a or new_a < a:
+        result = True
+    else:
+        result = False
     """
     The two-digit integer is given.
     Replace the digits of the number.
@@ -9,4 +19,4 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    return result
